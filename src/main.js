7fdc55f40cb0c893ee4df228667b4cc2ea625744
Dpbm <dpbm136@gmail.com>
2023-11-10 16:26:24 -0300

    updated dependencies

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
-const createScreenshotsFolder = require('./folderControl/controllerScreenshots');
-const getFirstPageLinks = require('./firstStepGetFirstPageLinks/firstStepGetFirstPageLinks');
-const createPagesFolder = require('./folderControl/controllerPages');
-const getInfoFromLinks = require('./secondStepGetLinksAndDownloadFiles/secondStepGetLinksAndDownloadFiles');
+const createScreenshotsFolder = require("./folderControl/controllerScreenshots");
+const getFirstPageLinks = require("./firstStepGetFirstPageLinks/firstStepGetFirstPageLinks");
+const createPagesFolder = require("./folderControl/controllerPages");
+const getInfoFromLinks = require("./secondStepGetLinksAndDownloadFiles/secondStepGetLinksAndDownloadFiles");
 
-async function main() {
-	await createScreenshotsFolder.createScreenshotsFolder();
-	const links = await getFirstPageLinks.getFirstPageLinks();
+(async function main() {
+    //await createScreenshotsFolder.createScreenshotsFolder();
+    //const links = await getFirstPageLinks.getFirstPageLinks();
 
-	console.log(links);
+    console.log("main");
 
-	await createPagesFolder.createPagesFolder();
-	await getInfoFromLinks.getInfoFromLinks(links);
+    //await createPagesFolder.createPagesFolder();
+    //await getInfoFromLinks.getInfoFromLinks(links);
 
-	process.exit(0);
-}
-
-module.exports.main = main;
+    process.exit(0);
+})();
